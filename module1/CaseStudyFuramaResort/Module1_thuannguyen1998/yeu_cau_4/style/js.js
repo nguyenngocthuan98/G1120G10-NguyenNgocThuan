a868afb2a75427055d8fbf927a790241c37ea4d9
nguyenngocthuan98 <vaicao321@gmail.com>
2020-12-22 15:20:00 +0700

    caseStudy yeu cau 4

---
+++ module1/CaseStudyFuramaResort/Module1_thuannguyen1998/yeu_cau_4/style/js.js
@@ -0,0 +1,38 @@
+let number;
+let arrNameCustomers = []; 
+let name_del;
+let flg = true;
+
+function addName() {
+    do {
+        input_number = prompt("Enter number of customers:");
+        if (!isNaN(input_number)) {
+            flg = false;
+        } else {
+            alert(input_number + " not a number.");
+        }
+    } while (flg);
+    number = parseInt(input_number);
+    console.log(number);
+    arrNameCustomers = Array(number);
+    for (let i = 0; i < arrNameCustomers.length; i++) {
+        arrNameCustomers[i] = prompt("Enter name customer:");
+    }
+    showList();
+}
+
+function delName() {
+    name_del = prompt("Enter name to delete: ");
+    for (let i = 0; i < arrNameCustomers.length; i++) {
+        if (arrNameCustomers[i] === name_del) {
+            arrNameCustomers.splice(i,1);
+        }
+    }
+    alert("Deleted customer: " + name_del);
+    showList();
+}
+
+function showList() {
+    document.getElementById("display").innerHTML =
+        "<h2>List Customers</h2>" + arrNameCustomers.sort().join("<br/>");
+}
