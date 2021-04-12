064b105a55067f8dac96d552a5d4ef35d483823e
nguyenngocthuan98 <37949712+nguyenngocthuan98@users.noreply.github.com>
2021-04-13 01:23:25 +0700

    M4 L11 UPDATE

--- module4/lesson11_web_service/exercise_blog_update/src/main/java/com/blog_manager_update/controllers/CategoryControllerAPI.java
+++ module4/lesson11_web_service/exercise_blog_update/src/main/java/com/blog_manager_update/controllers/CategoryControllerAPI.java
@@ -3,10 +3,11 @@ package com.blog_manager_update.controllers;
 import com.blog_manager_update.models.Category;
 import com.blog_manager_update.services.CategoryServiceAPI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -21,5 +22,43 @@ public class CategoryControllerAPI {
             return new ResponseEntity<List<Category>>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<List<Category>>(categoryServiceAPI.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/viewDetailCategoryAPI")
+    public ResponseEntity<Category> viewDetailCategory(@RequestParam Integer id) {
+        return new ResponseEntity<Category>(categoryServiceAPI.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/createCategory")
+    public ResponseEntity<Void> createCategory(@RequestBody Category category, UriComponentsBuilder uriComponentsBuilder) {
+        categoryServiceAPI.save(category);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uriComponentsBuilder.path("/categories/{id}").buildAndExpand(category.getId()).toUri());
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/updateCategory")
+    public ResponseEntity<Category> updateCategory(@RequestParam Integer id, @RequestBody Category category) {
+        Category currentCategory = categoryServiceAPI.findById(id);
+        if (currentCategory == null) {
+            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
+        } else {
+            currentCategory.setId(category.getId());
+            currentCategory.setName(category.getName());
+            currentCategory.setBlog(category.getBlog());
+
+            categoryServiceAPI.save(currentCategory);
+            return new ResponseEntity<Category>(currentCategory, HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/deleteCategory")
+    public ResponseEntity<Category> deleteCategory(@RequestParam Integer id) {
+        if (categoryServiceAPI.findById(id) == null) {
+            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
+        } else {
+            categoryServiceAPI.deleteById(id);
+            return new ResponseEntity<Category>(HttpStatus.NO_CONTENT);
+        }
     }
 }
