4e363598deda0894d8849d040a604d89617c2c28
nguyenngocthuan98 <37949712+nguyenngocthuan98@users.noreply.github.com>
2021-04-12 08:28:22 +0700

    M4 L10
    incomplete

---
+++ module4/lession10_session_cookie/exercise_cart/src/main/java/com/cart/exercise_cart/ExerciseCartApplication.java
@@ -0,0 +1,13 @@
+package com.cart.exercise_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseCartApplication.class, args);
+    }
+
+}
---
+++ module4/lession10_session_cookie/exercise_cart/src/test/java/com/cart/exercise_cart/ExerciseCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart.exercise_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
