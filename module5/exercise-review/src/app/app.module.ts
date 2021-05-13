826ee3e3c1fd128f43d57bcdf430bdcb7eb22ce1
nguyenngocthuan98 <37949712+nguyenngocthuan98@users.noreply.github.com>
2021-05-13 08:58:22 +0700

    M5 exercise-review UPDATE

--- module5/exercise-review/src/app/app.module.ts
+++ module5/exercise-review/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
+import {RouterModule} from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
